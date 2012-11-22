f1e174f46d88d60b9a5cceb2b69b1302b9e34a41
András Bánki Horváth <bhandras@gmail.com>
2012-11-22 17:16:03 +0100

    works on osx

--- projects/shooter/src/main_osx.cpp
+++ projects/shooter/src/main_osx.cpp
@@ -51,7 +51,7 @@ int main (int argc, char **argv)
 {
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(640, 480);
+	glutInitWindowSize(w, h);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("gl test");
 	glutDisplayFunc(myDisplay);
